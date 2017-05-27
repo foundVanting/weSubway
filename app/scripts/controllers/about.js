07fef94506bcea9a636551b518340c6fb9160f9c
huangdi <991396010@qq.com>
2017-05-27 15:28:29 +0800

    first

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name newsubwayApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the newsubwayApp
+ */
+angular.module('newsubwayApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name newsubwayApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the newsubwayApp
+ */
+angular.module('newsubwayApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
