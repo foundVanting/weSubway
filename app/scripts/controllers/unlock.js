c57c35947fcfc37fe798432364d4b6faf24d8b3d
huangdi <991396010@qq.com>
2017-06-01 14:56:30 +0800

    'fix'

--- app/scripts/controllers/unlock.js
+++ app/scripts/controllers/unlock.js
@@ -18,12 +18,12 @@ angular.module('newsubwayApp').controller('UnlockCtrl', ['$scope','$cookies','un
     $scope.scanQrCode=scanQrCode;
 
     function scanQrCode() {
-        alert(1)
         wx.scanQRCode({
-            needResult: 0, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+            needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
             scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
             success: function (res) {
                 console.log(res);
+                alert(res.resultStr);
                 var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
             }
         })
