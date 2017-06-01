e86b8b407453bd555e96f7c243f729f631ec00c7
huangdi <991396010@qq.com>
2017-06-01 17:22:41 +0800

    'fix'

--- app/scripts/controllers/unlock.js
+++ app/scripts/controllers/unlock.js
@@ -51,23 +51,39 @@ angular.module('newsubwayApp')
             sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
             success: function (res) {
                 var localIds = res.localIds[0].toString(); // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
-                setCertificateImage(localIds)
+
                 // uploadImage(localIds)
+                getLocalImgData(localIds)
 
             }
 
         });
     }
-    function uploadImage(localIds) {
-        wx.uploadImage({
-            localId: localIds, // 需要上传的图片的本地ID，由chooseImage接口获得
-            isShowProgressTips: 1, // 默认为1，显示进度提示
-            success: function (res){
-                var serverId = res.serverId; // 返回图片的服务器端ID
+    function getLocalImgData(localIds) {
+        wx.getLocalImgData({
+            localId: localIds, // 图片的localID
+
+            success: function (res) {
+
+                var localData = res.localData; // localData是图片的base64数据，可以用img标签显示
+                alert(localData);
+                setCertificateImage(localData)
+
             }
 
         });
+
     }
+    // function uploadImage(localIds) {
+    //     wx.uploadImage({
+    //         localId: localIds, // 需要上传的图片的本地ID，由chooseImage接口获得
+    //         isShowProgressTips: 1, // 默认为1，显示进度提示
+    //         success: function (res){
+    //             var serverId = res.serverId; // 返回图片的服务器端ID
+    //         }
+    //
+    //     });
+    // }
     function setQrCodeImage(data) {
         $scope.QrCodeImage =data;
     }
