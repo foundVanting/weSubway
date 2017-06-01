a82a612755fc63b32947f91d30be0c6454c58757
xiaolin <xiaolin@zan-xing.com>
2017-06-01 15:52:53 +0800

    remote bootstrap and jquery in index.html

--- app/scripts/services/unlockservice.js
+++ app/scripts/services/unlockservice.js
@@ -8,16 +8,18 @@
  * Service in the newsubwayApp.
  */
 angular.module('newsubwayApp')
-  .service('unlockService', ['$http',function ($http) {
-      return  {
-          getQrCode: getQrCode,
-          getGoodsList:getGoodsList
-      };
-      function getQrCode(equipNumber,goodsId,uid,companyId,certificate) {
-          return $http.get(Config.qrcode_url, {params:{"equinumber": equipNumber, "goodsId": goodsId,"companyId":companyId,"operatorId":uid,'certificate':certificate}});
-      }
-      function getGoodsList(channel_id) {
-          return $http.get(Config.url_prefix + "/channel/"+channel_id+"/goods");
+    .service('unlockService', ['$http', function($http) {
+        return {
+            getQrCode: getQrCode,
+            getGoodsList: getGoodsList
+        };
 
-      }
-  }]);
+        function getQrCode(equipNumber, goodsId, uid, companyId, certificate) {
+            return $http.get(Config.qrcode_url, { params: { "equinumber": equipNumber, "goodsId": goodsId, "companyId": companyId, "operatorId": uid, 'certificate': certificate } });
+        }
+
+        function getGoodsList(channel_id) {
+            return $http.get(Config.url_prefix + "/channel/" + channel_id + "/goods");
+
+        }
+    }]);
