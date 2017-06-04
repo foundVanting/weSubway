b7f8d50d01ed0aed3ec5674b8fc96aa31fb5450d
unknown <>
2017-06-04 15:04:27 +0800

    'fixbug'

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -18,7 +18,18 @@ function MainCtrl($scope, $cookies, mainService) {
     $scope.imageShowStatus=false;
     $scope.changeStatus = changeStatus;
     $scope.showImage = showImage;
+    $scope.checkImgExists = checkImgExists;
     getOrders();
+    function checkImgExists(imgurl) {
+        var ImgObj = new Image(); //判断图片是否存在
+        ImgObj.src = imgurl;
+        //没有图片，则返回-1
+        if (ImgObj.fileSize > 0 || (ImgObj.width > 0 && ImgObj.height > 0)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     function showImage(url) {
         $scope.imageUrl = url;
